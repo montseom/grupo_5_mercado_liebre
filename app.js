58bed46e04b0af42e31801958bb647fb01f9414c
montseom <molmedo.mancilla@gmail.com>
2021-06-16 20:39:47 -0500

    Se cambio a puerto 80

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-// const PORT = 3000;
+const PORT = process.env.PORT || 80
 
 app.use(express.static(__dirname+'/public'));
 
@@ -21,7 +21,7 @@ app.get('/crear-cuenta', (req, res) => {
 });
 
 
-app.listen({ port: process.env.PORT || 4000 }, () => {
-  console.log("Escuchando en http://localhost:" + 4000);
+app.listen(PORT, () => {
+  console.log("Escuchando en http://localhost:" + PORT);
 });
 
